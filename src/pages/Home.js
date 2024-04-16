ab8b14868d26cf7c60f70f131a9fc75e67d73bc0
youssef <yousseftaoussi894@gmail.com>
2024-04-16 09:09:22 +0100

    create custom hook that responsible oh highlight table content of current section that you're on

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,50 +5,36 @@ import Journey from "../components/Experiences";
 import Navbar from "../components/Navabar.js";
 import Contact from "../components/Contact";
 import Technologies from "../components/technologies.js";
+import useVisibleSection from "../hooks/useHeighlightContent.js";
 const Home = () => {
-  const [activeList, setActiveList] = useState("about");
   const aboutRef = useRef(null);
   const technologiesRef = useRef(null);
   const journeyRef = useRef(null);
   const projectsRef = useRef(null);
   const contactRef = useRef(null);
+  const [activeList, setActiveList] = useVisibleSection([
+    {
+      ref: aboutRef,
+      refName: "about",
+    },
+    {
+      ref: technologiesRef,
+      refName: "technologies",
+    },
+    {
+      ref: journeyRef,
+      refName: "journey",
+    },
+    {
+      ref: projectsRef,
+      refName: "projects",
+    },
+    {
+      ref: contactRef,
+      refName: "contact",
+    },
+  ]);
   const handleVisibleSection = (sectionName) => setActiveList(sectionName);
-  useEffect(() => {
-    const onScroll = (e) => {
-      let scrollTop = window.scrollY;
-      if (
-        contactRef.current &&
-        projectsRef.current &&
-        journeyRef.current &&
-        aboutRef.current &&
-        technologiesRef.current
-      ) {
-        if (
-          aboutRef.current.offsetTop < scrollTop &&
-          scrollTop < technologiesRef.current.offsetTop - 250
-        )
-          setActiveList("about");
-        else if (
-          technologiesRef.current.offsetTop < scrollTop &&
-          scrollTop < journeyRef.current.offsetTop - 250
-        )
-          setActiveList("technologies");
-        else if (
-          journeyRef.current.offsetTop < scrollTop &&
-          scrollTop < projectsRef.current.offsetTop - 250
-        )
-          setActiveList("journey");
-        else if (
-          projectsRef.current.offsetTop - 100 < scrollTop &&
-          scrollTop < contactRef.current.offsetTop - 250
-        )
-          setActiveList("projects");
-        else setActiveList("contact");
-      }
-    };
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
   return (
     <div className="homeContainer  z-20 overflow-x-hidden font-mono">
       <Navbar activeList={activeList} />
